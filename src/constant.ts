9c978f7f6fd4101abd2ed4afe36e12e5240f1fe7
Colin Cheng <49746060+collincheng@users.noreply.github.com>
2025-12-03 15:09:44 +0800

    feat: add absoluteWorkspaceFolder constant to resolve workspace path

--- src/constant.ts
+++ src/constant.ts
@@ -1,1 +1,8 @@
-export const CONFIG_FILE_NAME = 'YAPI';
+import path from "path";
+import * as vscode from "vscode";
+
+export const CONFIG_FILE_NAME = "YAPI";
+
+export const absoluteWorkspaceFolder = path.resolve(
+  vscode.workspace.workspaceFolders?.[0]?.uri.fsPath || process.cwd()
+);
